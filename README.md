04c0e9474e3419d6e300879cbef9d15938261551
sharon wakade <128405377+sharonwakade@users.noreply.github.com>
2023-06-23 17:51:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # React_Resume_Bilder
-React_Resume_Bilder This is a Resume Builder Project Created using React js
+
+This is a Resume Builder Project Created using React js
+
+# #React js #JSPDF #HTML2CANVAS
---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import Builder from './components/Builder';
+
+
+const  App = () => {
+
+  return(
+
+  <>
+        <div className="container">
+          <Builder/>
+        </div>
+  </>
+  );
+}
+
+export default App;
